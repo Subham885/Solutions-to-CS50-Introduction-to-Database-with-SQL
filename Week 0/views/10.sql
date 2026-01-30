37de27ab159507bc920b19e079665ccc6cb63337
Subham <subhamgupta885@gmail.com>
2026-01-30 19:55:41 +0530

    Add Solution for views

---
+++ Week 0/views/10.sql
@@ -0,0 +1,1 @@
+SELECT "japanese_title" AS 'JAPAN' FROM "views" WHERE "artist" LIKE 'hokusai' ORDER BY "entropy" DESC;
---
+++ Week 0/views/8.sql
@@ -0,0 +1,1 @@
+SELECT "english_title" FROM "views" WHERE "artist" LIKE 'hokusai' ORDER BY "contrast" ASC LIMIT 5;
